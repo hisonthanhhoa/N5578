78e6cf623e5df0f5d194df5fd513640fc1c2e165
Hoàng Tiến Sơn <77462107+hisonthanhhoa@users.noreply.github.com>
2023-05-25 23:32:46 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hello là Son đây!',
+    introDesc: `Chào tất cả mọi người, tự nhiên từ xa lạ lại được gặp mọi người, xong gắn bó với nhau 1 thời gian. Chúc mọi người sau bài test N5 thì ai có dự định gì thì cố gắng thực hiện, còn những ai theo N4 thì chúng ta cố gắng nhé`,
+    btnIntro: 'hihi',
+    title: 'Lớp học của chúng ta rất vui vẻ đúng không mọi người 😙',
+    desc: 'Ai mà giàu thì nhớ giữ liên lạc với lớp nhé ',
+    btnYes: 'giữ liên lạc và sẽ tự động gửi Sơn 500k ',
+    btnNo: 'Quên phát một :3',
+    question: 'Chúng ta gặp nhau ở nhau ở RiKi tuy không dài nhưng mà cũng vui mà đúng hông. Gửi lời yêu thương vào đây nhé😘😘 ',
+    btnReply: 'Gửi <3',
+    reply: 'Chúc tất cả thành công! đừng cố gắng nhập nữa, không nhập được đâu, với một điều quan trọng là thằng cha làm ra cái này [dangeo]',
+    mess: 'Tôi biết các ngươi muốn nói gì mà, không cần xúc động đâu',
+    messDesc: 'Hẹn gặp lại mọi người ở N4 hoặc bên Nhật nhé .',
+    btnAccept: 'arigatou',
+    messLink: 'https://www.facebook.com/groups/1200934503863681/members/'
+}
